2ce7cce1388fe0b80c3a208e2eda71f239cc2381
assayw119 <assayw119@naver.com>
2022-08-23 21:17:27 +0900

    여름세션 7주차 과제

--- SummerSession7/session7_homework/session/crud/views.py
+++ SummerSession7/session7_homework/session/crud/views.py
@@ -1,13 +1,13 @@
-from telnetlib import STATUS
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from .models import Movie, Review
 from .serializer import MovieListSerializer, ReviewListSerializer
 from django.shortcuts import get_list_or_404, render, get_object_or_404
-
-# from session.crud import serializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.contrib.auth import authenticate
 
 @api_view(['GET','POST'])
+@permission_classes([IsAuthenticated])
 def movie_list_create(request):
     if request.method == 'GET':
         movies = Movie.objects.all()
@@ -23,6 +23,7 @@ def movie_list_create(request):
             return Response(data=serializer.data)
 
 @api_view(['GET','PATCH','DELETE'])
+@permission_classes([IsAuthenticated])
 def movie_detail_update_delete(request, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
 
@@ -44,6 +45,7 @@ def movie_detail_update_delete(request, movie_pk):
         return Response(data)
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def review_list_create(request, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
     if request.method == 'GET':
@@ -59,6 +61,7 @@ def review_list_create(request, movie_pk):
         return Response(data=serializer.data)
 
 @api_view(['GET','PATCH','DELETE'])
+@permission_classes([IsAuthenticated])
 def review_detail(request, review_pk):
     review = get_object_or_404(Review, pk=review_pk)
 
--- SummerSession7/session7_homework/session/session/settings.py
+++ SummerSession7/session7_homework/session/session/settings.py
@@ -39,7 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crud',
     'rest_framework',
+    'accounts',
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "accounts.authentications.JWTAuthentication",
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
